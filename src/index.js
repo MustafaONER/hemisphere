a63f930a39da8f2068353b8992cad3b8c4eede6f
Mustafa <mustizena@gmail.com>
2022-06-30 03:27:20 +0200

    first commit

--- src/index.js
+++ src/index.js
@@ -1,8 +1,35 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import HemisphereDisplay from './HemisphereDisplay';
+
+
+class App extends React.Component {
+
+  state = { latitude: null, errorMessage: '' };
+
+  componentDidMount() {
+    window.navigator.geolocation.getCurrentPosition(
+      (position) => {
+        this.setState({ latitude: position.coords.latitude });
+      },
+      (error) => {
+        this.setState = { errorMessage: error.message };
+      }
+    );
+  }
+
+  render() {
+    if (this.state.errorMessage && !this.state.latitude) {
+      return <div>{this.state.errorMessage}</div>;
+    }
+    if (this.state.latitude && !this.state.errorMessage) {
+      return <div><HemisphereDisplay latitude={this.state.latitude} /> </div>;
+    } else {
+      return <div>Loading...</div>;
+    }
+  }
+}
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -10,8 +37,3 @@ root.render(
     <App />
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
